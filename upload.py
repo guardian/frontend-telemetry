25448eb98577987e36361f70872d5acf84fcae1c
Matt Osborn <matthew.osborn@theguardian.com>
2015-02-19 10:29:04 +0000

    naming

--- upload.py
+++ upload.py
@@ -6,7 +6,8 @@ import os
 import calendar
 import json
 
-benchmark_names = ['ContentAll','FrontsAll']
+BENCHMARK_NAMES = ['ContentAll','FrontsAll']
+TABLE_NAME = 'perf'
 
 def iso_timetsamp_to_unix(iso_timestamp):
 	datetime = dateutil.parser.parse(iso_timestamp)
@@ -20,7 +21,7 @@ client = boto3.client('dynamodb')
 iso_timestamp = sys.argv[1]
 unix_timestamp = iso_timetsamp_to_unix(iso_timestamp)
 
-for test_name in benchmark_names:
+for test_name in BENCHMARK_NAMES:
 	results_filename = os.path.join('results', iso_timestamp, test_name, 'results.json')
 	with open(results_filename, 'r') as f:
 		benchmark_data = f.read()
@@ -30,4 +31,4 @@ for test_name in benchmark_names:
 			'benchmark_name': { 'S': benchmark_json['benchmark_name'] },
 			'benchmark_data': { 'S': benchmark_data }
 		}
-		client.put_item(TableName='perf', Item=item)
+		client.put_item(TableName=TABLE_NAME, Item=item)
